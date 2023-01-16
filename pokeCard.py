9f4a50dd78f732ea7ffe6978d16808077baa9bf3
terrym <terrencepmoran@gmail.com>
2023-01-16 12:57:07 -0600

    PokeCard optimization and added statIVs as a combined widget to the picture label

--- pokeCard.py
+++ pokeCard.py
@@ -13,15 +13,28 @@ class PokeCard(qtw.QWidget):
 
 
         name_label = qtw.QLabel(f"No. {self.poke.Number}: {self.poke.Name}")
-        name_label.setFont(qtg.QFont("Ariel", 14, 2))
+        name_label.setFont(qtg.QFont("Ariel", 12, 2))
         layout.addWidget(name_label)
 
+        stat_img = qtw.QWidget()
+        stats_img_layout = qtw.QHBoxLayout()
+        stats_img_layout.setAlignment(qtc.Qt.AlignmentFlag.AlignCenter)
+        stats_label = self.PokeCardIVs(self.poke)
 
-        picture_label = WBImages(self.poke.img, 100, True)
-        layout.addWidget(picture_label)
+
+        picture_label = WBImages(self.poke.img, 150, True)
+        
+        stats_img_layout.addWidget(stats_label)
+        stats_img_layout.addWidget(picture_label)
+
+        stat_img.setLayout(stats_img_layout)
+
+        layout.addWidget(stat_img)
+
         
         types = self.TypeImages(self.poke.Type1, self.poke.Type2)
         layout.addWidget(types)
+
 
 
         moves_list = qtw.QListWidget()
@@ -46,22 +59,35 @@ class PokeCard(qtw.QWidget):
         self.setParent(None)
         self.deleteLater()
     
-    class IVStats(qtw.QWidget):
-        def __init__(self, parents = None):
-            super().__init__(parents)
-
-
     class TypeImages(qtw.QWidget):
         def __init__(self, type1, type2=None, parent=None):
             super().__init__(parent)
             layout = qtw.QHBoxLayout()
             layout.setAlignment(qtc.Qt.AlignmentFlag.AlignHCenter)
-            type1_label = WBImages(f"images\\sm_icon\\{type1}_sm_icon.png", 50)
-            type2_label = WBImages(f"images\\sm_icon\\{type2}_sm_icon.png", 50)
+            type1_label = WBImages(f"images\\sm_icon\\{type1}_sm_icon.png", 75)
+            type2_label = WBImages(f"images\\sm_icon\\{type2}_sm_icon.png", 75)
             layout.addWidget(type1_label)
             if (type2 != ""):
                 layout.addWidget(type2_label)
             self.setLayout(layout)
+
+    class PokeCardIVs(qtw.QWidget):
+        def __init__(self,poke):
+            super().__init__()
+            layout = qtw.QHBoxLayout()
+            stats = ["Speed", "SpAtk", "SpDef", "Attack", "Defense", "HP"]
+            for stat in stats:
+                setattr(self, stat, getattr(poke, stat))
+            poke_stat = ""
+            IV_label = qtw.QLabel()
+            for stat in stats:
+                poke_stat += f"{stat}\n{str(getattr(self, stat))}\n"
+            IV_label.setText(poke_stat)
+            layout.addWidget(IV_label)
+            self.setLayout(layout)
+            
+
+
 
 
 
