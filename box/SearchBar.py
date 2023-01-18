54c12c4228bd1d06703eecdb1c7845b2e1c8f985
barelyscaryterry <terrencepmoran@gmail.com>
2023-01-17 21:04:21 -0600

    Added pictures to the search bar.  Yet to develop that class...

--- box/SearchBar.py
+++ box/SearchBar.py
@@ -2,41 +2,41 @@ from PyQt5 import QtWidgets
 from classes.Pokemon import Pokemon
 from classes.WBImages import WBImages
 
-
 class SearchBar(QtWidgets.QWidget):
-    class SearchBoxes(QtWidgets.QWidget):
-
-        def __init__(self, mon):
-            super().__init__()
-            self.mon = mon
-            self.img = WBImages(self.mon.img, 50, True)
-            self.add_button = QtWidgets.QPushButton("Add", self.img)
     def __init__(self, dataset = None):
         super().__init__()
-
         self.dataset = dataset
         self.query_results = []
+        self.results_view = QtWidgets.QWidget()
 
         self.search_bar = QtWidgets.QLineEdit(self)
         self.search_bar.textChanged.connect(self.on_text_changed)
-
-        
-        results_widg = QtWidgets.QColumnView()
-        self.results_layout = QtWidgets.QHBoxLayout()
-        results_widg.setLayout(self.results_layout)
-        
-
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(self.search_bar)
+       
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.search_bar)
         
 
     def on_text_changed(self, text):
+       
         query = text.lower()
         self.query_results = [
             (name, self.dataset[name]) for name in self.dataset
             if query in name.lower()
         ]
+
         self.query_results = self.query_results[:10]
-        for name in self.query_results:
-            pk = Pokemon(name[1])
-            self.results_layout.addWidget(self.SearchBoxes(pk))
+        self.layout.removeWidget(self.results_view)
+        self.results_view.deleteLater()
+        self.results_view = QtWidgets.QWidget()
+        query_images_layout_first5 = QtWidgets.QHBoxLayout()
+        
+        if len(self.query_results) != 0:
+            for name in self.query_results:
+                pk = Pokemon(name[1])
+                pokeimg = WBImages(pk.img, 70, True)
+                query_images_layout_first5.addWidget(pokeimg)
+        else:
+            query_images_layout_first5.addWidget(QtWidgets.QLabel("None found"))
+        
+        self.results_view.setLayout(query_images_layout_first5)
+        self.layout.addWidget(self.results_view)
